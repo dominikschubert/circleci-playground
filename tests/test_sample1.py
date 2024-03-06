33aed98d229b2dbfd3e20183fb98377ebf0ae3fd
Dominik Schubert <dominik.schubert91@gmail.com>
2024-03-06 09:31:58 +0100

    mix up test durations

--- tests/test_sample1.py
+++ tests/test_sample1.py
@@ -1,10 +1,10 @@
 import time
 
 def test_sample1_test1():
-    time.sleep(1)
+    time.sleep(10)
     assert 1 == 1
 
 
 def test_sample1_test2():
-    time.sleep(1)
+    time.sleep(10)
     assert 1 == 1
--- tests/test_sample4.py
+++ tests/test_sample4.py
@@ -1,10 +1,10 @@
 import time
 
 def test_sample4_test1():
-    time.sleep(4)
+    time.sleep(10)
     assert 1 == 1
 
 
 def test_sample4_test2():
-    time.sleep(4)
+    time.sleep(10)
     assert 1 == 1
